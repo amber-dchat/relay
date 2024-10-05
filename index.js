13a3a1086bf30c9539a0d8eff9d2dd28b3378865
Retro <70205403+retrouser955@users.noreply.github.com>
2024-10-05 10:41:11 +0630

    implement cors

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const express = require("express");
+const cors = require("cors")
 const Gun = require("gun");
 
+// WARNING: DO NOT OVERRIDE
+const isDevelopment = (process.env.NODE_ENV || "development") === "development"
+const allowedOrigin = isDevelopment ? "http://localhost:3000" : "https://amber-dchat.github.io"
+
 const app = express();
+
+app.use(cors({
+    origin: allowedOrigin
+}))
 
 const PORT = process.env.PORT || 3000;
 
@@ -11,4 +20,4 @@ app.get("/", (_, res) => {
 
 Gun({ web: app, radisk: true });
 
-app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
+app.listen(PORT, () => console.log(`Listening on port: ${PORT}\nCORS: Only allowing ${allowedOrigin}`));
